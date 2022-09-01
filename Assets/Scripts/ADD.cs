1124a55241602e6f18ee882b49eb7867c638b6ca
Rusheel Dasari <rusheel@gmail.com>
2022-08-31 21:56:20 -0700

    Added a script and some jsons

---
+++ Assets/Scripts/ADD.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ADD : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // define inputs
+        var input1 = 0;
+        var input2 = 0;
+        // load the logic
+        // define the outputs
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // read inputs
+        // send outputs
+    }
+}
