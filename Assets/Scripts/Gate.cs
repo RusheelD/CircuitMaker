d31da3a2cfc9be5077d5c2a7b29345ac9f4cb4aa
Rusheel Dasari <rusheel@gmail.com>
2022-09-02 16:25:17 -0700

    Continued working on logic for gates

--- Assets/Scripts/Gate.cs
+++ Assets/Scripts/Gate.cs
@@ -1,3 +1,4 @@
+using System.Net.NetworkInformation;
 using System.Globalization;
 using System.Collections;
 using System.Collections.Generic;
@@ -8,11 +9,36 @@ public class Gates : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        gameObject.name = "NAND"
+        Read file;
+        Chip[] chips = new Chip[];
         using (StreamReader reader = new StreamReader("../Chips/" + gameObject.name + ".json"))
         {
             string json = reader.ReadToEnd();
+            file = JsonSerializer.Deserialize<Read>(json);
         }
+        foreach (var (index, gate) in file.gates) {
+            gate.id = index;
+            if(gate.type == "AND") {
+                chips[index] = new And();
+                chips[index].input1 = gate.from[0];
+                chips[index].input2 = gate.from[1];
+            } else if(gate.type == "NOT") {
+                chips[index] = new Not();
+                chips[index].input = gate.from[0];
+            } else if(gate.type == "INPUT") {
+                chips[index] = new Input();
+            } else if(gate.type == "OUTPUT") {
+                chips[index] = new Output();
+                chips[index].input = gate.from[0];
+            }
+            if(chips[index]) {
+                chips[index].id = gate.id;
+                chips[index].type = gate.type; 
+                chips[index].chips = chips; 
+            }
+        }
+
         // define inputs
         // load the logic
         // define the outputs
@@ -31,28 +57,34 @@ public class Gates : MonoBehaviour
     }
 
     private class Chip {
-        public int id;
-        public string name;
-        public int[] from = [];
+        public int id { get; set;};
+        public string type { get; set; };
+        public int[] from { get; set; } = [];
+        public Chip[] chips { get; set; } = null;
     }
 
     private class Add : Chip {
-        public int input1;
-        public int input2;
-        public int output;
-
+        public int input1 { get; set;};
+        public int input2 { get; set;};
+        public int output { get; set;};
+        public void updateOutput() {
+            output = chips[input1].output == 1 && chips[input2].output == ? 1 : 0;
+        }
     }
 
     private class Not : Chip {
-        public int input;
-        public int output;
+        public int input { get; set;};
+        public int output { get; set;};
+        public void updateOutput() {
+            output = abs(chips[input].output - 1);
+        }
     }
 
     private class Input : Chip {
-        public int output;
+        public int output { get; set;};
     }
 
     private class Output : Chip {
-        public int input;
+        public int input { get; set;};
     }
 }
